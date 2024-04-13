47226139a81a498f375127f185206e07e699cde7
Ben888100 <benhartline88@gmail.com>
2024-04-12 21:58:27 -0400

    new stuff

--- GUI_test.py
+++ GUI_test.py
@@ -1,45 +1,5 @@
 import tkinter as tk
-import pandas as pd
-import time
-
-import Manage_Data
-
-# Initialize variables
-space_pressed = False
-start_time = time.time()
-intervals = []
-
-begin_time = time.time()
-
-def on_key_press(event):
-    global space_pressed, start_time
-
-    if event.keysym == "space":
-        end_time = time.time()
-        
-        elapsed_time = end_time - start_time
-
-        start_time = time.time()
-        intervals.append(elapsed_time)
-        print(f"Elapsed time: {elapsed_time:.2f} seconds")
-        print(f"Total Time: {time.time()-begin_time:.2f} seconds")
-
-def display_array():
-    filtered_data = Manage_Data.filter(intervals)
-    BPS = Manage_Data.calculateBPS(filtered_data)
-    print(BPS)
-
-# Create the root window
-root = tk.Tk()
-root.title("Space Bar Timer")
-
-# Bind the space bar key press event
-root.bind("<KeyPress>", on_key_press)
-
-# Add a button to display the array
-array_button = tk.Button(root, text="Display Array", command=display_array)
-array_button.pack()
-
+from PIL import ImageTk, Image
 
 def on_click(event):
     global counter
@@ -79,8 +39,8 @@ round_button.create_image(100, 100, image=heart_icon)
 counter_text = round_button.create_text(60, 90, text=str(counter), font=("Arial", 24))
 
 # Other text
-text_label = tk.Label(root, text="Hello, world!", font=("Arial", 24), fg="blue", bg="white", justify="center")
-text_label.pack(pady=20)  # Adjust padding as needed
+#text_label = tk.Label(root, text="Hello, world!", font=("Arial", 24), fg="blue", bg="white", justify="center")
+#text_label.pack(pady=20)  # Adjust padding as needed
 
 # Bind events
 round_button.bind('<Button-1>', on_click)
@@ -91,4 +51,3 @@ round_button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
 # Start the GUI event loop
 root.mainloop()
-
