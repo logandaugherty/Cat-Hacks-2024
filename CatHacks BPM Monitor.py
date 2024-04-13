daf05526ed74a870f382bece32d53d333579d880
Superkman123 <kevinbuin@gmail.com>
2024-04-13 11:37:56 -0400

    Update CatHacks BPM Monitor.py

--- CatHacks BPM Monitor.py
+++ CatHacks BPM Monitor.py
@@ -4,87 +4,90 @@ print("To start, enter your age and then enter if you are recording your resting
 print("Then, feel your pulse with one hand and start pressing the spacebar everytime you feel a pulse.")
 print("Enjoy!\n")
 
-# # Testing Code
-age = input("Enter your age: ")
-age = int(age)
-# if (age < 10):
-    # print("You are too young to use this app!")
+# Testing Code
+age = int(input("Enter your age: "))
+
+while (age < 10):
+    age = int(input("\nYou are too young to use this app!\nPlease enter a valid age: "))
+
 heartType = input("Are you recording your resting and moving heart rate? (Please type \"R\" for resting and \"M\" for moving). ")
 
-BPM = 300
+# Replace with actual BPM data
+BPM = 100
 
-low = True
-
+# Function to determine if heart rate is healthy or not
 def determine_heart_cond(age, heartType, BPM):
-    if (heartType == "R"):
+    if (heartType == "R" or heartType == "r"):
         if (age >= 10):
             if (BPM  >= 60 and BPM <= 100): 
-                print("Congrats! You have a healthy heart rate.")
-            elif (BPM >= 100):
-                low = False
-    elif (heartType == "M"):
+                healthy = True
+            else:
+                healthy = False
+    elif (heartType == "M" or heartType == "m"):
         if (age >= 20):
             if (BPM  >= 100 and BPM <= 170):
-                print("Congrats! You have a healthy heart rate.")
-            elif (BPM >= 170):
-                low = False
+                healthy = True
+            else:
+                healthy = False
         elif (age >= 30):
             if (BPM  >= 95 and BPM <= 162):
-                print("Congrats! You have a healthy heart rate.")
-            elif (BPM >= 162):
-                low = False
+                healthy = True
+            else:               
+                healthy = False
         elif (age >= 35):
             if (BPM  >= 93 and BPM <= 157):
-                print("Congrats! You have a healthy heart rate.")
-            elif (BPM >= 157):
-                low = False
+                healthy = True
+            else:               
+                healthy = False
         elif (age >= 40):
             if (BPM  >= 90 and BPM <= 153):
-                print("Congrats! You have a healthy heart rate.")
-            elif (BPM >= 153):
-                low = False
+                healthy = True
+            else:              
+                healthy = False
         elif (age >= 45):
             if (BPM  >= 88 and BPM <= 149):
-                print("Congrats! You have a healthy heart rate.")
-            elif (BPM >= 149):
-                low = False
+                healthy = True
+            else:                
+                healthy = False
         elif (age >= 50):
             if (BPM  >= 85 and BPM <= 145):
-                print("Congrats! You have a healthy heart rate.")
-            elif (BPM >= 145):
-                low = False
+                healthy = True
+            else:               
+                healthy = False
         elif (age >= 55):
             if (BPM  >= 83 and BPM <= 140):
-                print("Congrats! You have a healthy heart rate.")
-            elif (BPM >= 140):
-                low = False
+                healthy = True
+            else:                
+                healthy = False
         elif (age >= 60):
             if (BPM  >= 80 and BPM <= 136):
-                print("Congrats! You have a healthy heart rate.")
-            elif (BPM >= 136):
-                low = False
+                healthy = True
+            else:                
+                healthy = False
         elif (age >= 65):
             if (BPM  >= 78 and BPM <= 132):
-                print("Congrats! You have a healthy heart rate.")
-            elif (BPM >= 132):
-                low = False
+                healthy = True
+            else:                
+                healthy = False
         elif (age >= 70):
             if (BPM  >= 75 and BPM <= 128):
-                print("Congrats! You have a healthy heart rate.")
-            elif (BPM >= 128):
-                low = False
-    return low
+                healthy = True
+            else:                
+                healthy = False
+    return healthy
 
-def high_and_low(low):
-    if (low == True):
-        print("\nYour heart rate is too low! Please do the following to increase heart rate:")
-        print("Exercise \nGet more sleep \nIf your BPM is still too low, please go see a doctor")
+
+# Function to provide health advice based on heart rate
+def advice(healthy):
+    if (healthy == True):
+        print("\nCongrats! You have a healthy heart rate.")
     else:
-        print("\nYour heart rate is too high! Please do the following to decrease heart rate:")
-        print("Meditate \nTake deep breaths \nExercise more often \nStart a healthier diet")
-        print("If you are still worried, please go see a doctor")
+        print("\nYour heart rate is abnormal for your age.\nPlease continue recording data and consult a medical professional")
 
-# test
-low = determine_heart_cond(age, heartType, BPM)
-high_and_low(low)
+# Test heart rate and provide advice
+healthy = determine_heart_cond(age, heartType, BPM)
+advice(healthy)
+
+# Inform user that data has been recorded
+print("\nYour data has been recorded.")
 
